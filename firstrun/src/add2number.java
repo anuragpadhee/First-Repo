4b9187c3a2d2cad1f28a81d25e5e2f75666861af
Anurag Padhee <127174026+anuragpadhee@users.noreply.github.com>
2025-06-07 23:17:46 +0530

    Add files via upload

---
+++ firstrun/src/add2number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class add2number {
+    public static void main(String[] args) {
+        {
+            System.out.println("enter 2 numer");
+            Scanner sc= new Scanner(System.in);
+            int a= sc.nextInt();
+            int b=sc.nextInt();
+            int c=a+b;
+            System.out.println("sum of 2 numbers ="+ c);
+        }
+    }
+}
---
+++ firstrun/src/explicittypecasting.java
@@ -0,0 +1,13 @@
+public class explicittypecasting {
+    public static void main(String [] args)
+    {
+        float myfloat=5;
+        System.out.print("before conversion="+ myfloat);
+
+        int myInt= (int)5.4f;
+        System.out.println("current scenario="+ myInt);
+
+
+
+    }
+}
